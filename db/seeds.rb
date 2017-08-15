f8dac8fbadb858ae36580ed804d5ed23fe35fc7b
augustin poupard <augustin.poupard@gmail.com>
2017-08-15 10:37:55 +0200

    add  booking  and  user with out book in the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Erasing all the DB'
-User.destroy_all
 
 puts 'Seeding your app'
-status = ['pending', 'cancelled', 'valided', 'finished']
+
+Booking.delete_all
+User.delete_all
+Product.delete_all
 
 10.times do
+
   a = User.new(
               first_name: (Faker::Name.first_name),
               last_name: (Faker::Name.last_name),
@@ -27,36 +30,49 @@ status = ['pending', 'cancelled', 'valided', 'finished']
               password_confirmation: 'tototo'
               )
 
-   (1..3).to_a.sample.times do
-       a.products.new(
-                 first_name: (Faker::Cat.name),
-                 last_name: (Faker::Name.last_name),
-                 date_of_birth: Faker::Date.birthday(18, 33),
-                 phone_number: Faker::PhoneNumber.phone_number,
-                 postal_code: Faker::Address.postcode,
-                 city: Faker::Address.city,
-                 lat: Faker::Address.latitude,
-                 lng: Faker::Address.longitude,
-                 address: Faker::Address.street_address,
-                 price: Faker::Commerce.price,
-                 hair: "blond",
-                 eyes: "blue",
-                 weight: 50,
-                 height: 1.88,
-                 shoes: 39,
-                 description: Faker::Lorem.paragraph
-                 )
+  (1..3).to_a.sample.times do
+    a.products.new(
+                   first_name: (Faker::Cat.name),
+                   last_name: (Faker::Name.last_name),
+                   date_of_birth: Faker::Date.birthday(18, 33),
+                   phone_number: Faker::PhoneNumber.phone_number,
+                   postal_code: Faker::Address.postcode,
+                   city: Faker::Address.city,
+                   lat: Faker::Address.latitude,
+                   lng: Faker::Address.longitude,
+                   address: Faker::Address.street_address,
+                   price: Faker::Commerce.price,
+                   hair: "blond",
+                   eyes: "blue",
+                   weight: 50,
+                   height: 1.88,
+                   shoes: 39,
+                   description: Faker::Lorem.paragraph
+                   )
 
-         end
-        a.save!
-      end
+    a.save!
+  end
 
-                #a = Reservation.new(
-                             # start_date: Faker::Date.forward(100),
-                             # end_date: Faker::Date.forward(100),
-                             # status: status.sample,
-                              #description:Faker::Lorem.paragraph
-                              #)
+  a = User.new(
+              first_name: (Faker::Name.first_name),
+              last_name: (Faker::Name.last_name),
+              email: Faker::Internet.email,
+              password: 'tototo',
+              password_confirmation: 'tototo'
+              )
+  a.save!
+
+  a = Booking.new(
+                  start_date: Time.now,
+                  end_date: Time.now,
+                  status: ['pending', 'cancelled', 'valided', 'finished'].sample,
+                  description:Faker::Lorem.paragraph,
+                  product_id:Product.all.sample.id,
+                  user_id:User.all.sample.id
+                  )
+  a.save!
+
+end
 
 puts 'Seed Completed'
 
