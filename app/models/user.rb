79122e266737ce2138cbe866a147000511c842e8
Carl Quivron <carl.quivron@gmail.com>
2017-08-18 11:23:40 +0200

    fix profil data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, PhotoUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :products, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :products_bookings, through: :products, source: :bookings, dependent: :destroy
   has_many :reviews
-  mount_uploader :avatar, PhotoUploader
 end
